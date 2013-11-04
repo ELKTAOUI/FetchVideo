c1ab1311afe27e443f6449e8998d7d9d531163ec
EL_KTAOUI Zakaria <elktaoui@outlook.com>
2013-11-04 02:30:59 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,26 @@
+
+<meta charset="utf-8">
+<?php
+
+require_once "stat.php";
+
+//$tab4=FetchVideo::getInfoYoutubeChanel('UCqyku6Kdofy1nV86yh9R8n8A');
+//$tab3=FetchVideo::getInfVimeoChanel('delicioussandwich');
+$tab2=FetchVideo::getInfoVimeo('7811489');
+$tab1=FetchVideo::getInfoYoutube('89ScoHpaZaw');
+
+foreach ($tab2 as $key => $value) {
+	if(is_array($value))
+	{
+		foreach ($value as $k => $v) {
+			# code...
+			echo $k.' : '.$v.'<br />';
+		}
+	}
+	else {
+	echo $key.' : '.$value.'<br />';
+	}
+}
+echo "<hr/>";
+
+ ?>
